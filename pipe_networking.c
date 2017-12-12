e4bc29544878fe97276e5393ecb57bacb592e0f7
Gingernetics <janstefan.kowalski@gmail.com>
2017-12-12 13:02:05 -0500

    Some work

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,13 +11,19 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  mkfifo("stc", 0600);
-  int fd = open("stc", O_RDONLY | O_CREAT | O_EXCL, 0600);
-  char * buff;
-  read(fd, buff, sizeof(int *));
-  printf("received fd: %d\n", atoi(buff));
-  close(fd);
-  return 0;
+ 	mkfifo("wkp", 0600);
+  	int wkp = open("wkp", O_RDONLY | O_CREAT | O_EXCL, 0600);
+	char * client_stream;
+  	read(wkp, client_stream, sizeof(int *));
+  	printf("received message through wkp: %d\n", atoi(client_stream));
+	close(wkp);
+
+	mkfifo(client_stream, 0600);
+	*to_client = open("stc", L, 0600);
+	write(
+
+
+  	return 0;
 }
 
 
@@ -31,9 +37,13 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  mkfifo("cts", 0600);
-  int wkp = open("stc", O_WRONLY);
-  *to_server = open("cts", O_CREAT | O_RDWR | O_EXCL, 0600);
-  write(wkp, to_server, sizeof(int *));
-  return 0;
+  	mkfifo("cts", 0600);
+  	int wkp = open("wkp", O_WRONLY);
+  	*to_server = open("cts", O_CREAT | O_RDWR | O_EXCL, 0600);
+  	write(wkp, to_server, sizeof(int *));
+
+	read(
+
+ 	return 0;
 }
+
