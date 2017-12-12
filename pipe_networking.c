673a349d57e89f954bf1c2c3012725261867e8db
Ida <iwang2@stuy.edu>
2017-12-12 11:02:06 -0500

    stuff from class

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,6 +11,12 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
+  mkfifo("stc", 0600);
+  int fd = open("stc", O_RDONLY | O_CREAT | O_EXCL, 0600);
+  char * buff;
+  read(fd, buff, sizeof(int *));
+  printf("received fd: %d\n", atoi(buff));
+  close(fd);
   return 0;
 }
 
@@ -25,5 +31,9 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
+  mkfifo("cts", 0600);
+  int wkp = open("stc", O_WRONLY);
+  *to_server = open("cts", O_CREAT | O_RDWR | O_EXCL, 0600);
+  write(wkp, to_server, sizeof(int *));
   return 0;
 }
